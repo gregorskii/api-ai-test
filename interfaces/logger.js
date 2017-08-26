240c9b9aa52a323dad92bf22ca4bc27e2c5892a0
Gregory Parsons <gregorskii@gmail.com>
2017-08-26 14:13:13 -0700

    Update logger.js

--- interfaces/logger.js
+++ interfaces/logger.js
@@ -2,15 +2,20 @@ const bunyan = require('bunyan');
 const bformat = require('bunyan-format');
 const formatOut = bformat({ outputMode: 'short' });
 
-module.exports = (silence) => {
+module.exports = (silence = false, level) => {
   const log = bunyan.createLogger({
     'name': 'apiai',
     stream: formatOut
   });
 
-  if (!silence || process.env.SILENCE !== '') {
-    log.level(bunyan.FATAL + 1)
+  if (level || process.env.LEVEL) {
+    const lv = bunyan[level] || bunyan[process.env.LEVEL];
+    log.level(lv);
   }
-  
+
+  if (silence || process.env.SILENCE !== '') {
+    log.level(bunyan.FATAL + 1);
+  }
+
   return log;
 };
